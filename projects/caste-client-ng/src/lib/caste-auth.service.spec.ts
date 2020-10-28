f03cdb31f67f8901a8130e4934cf69cdb17eaba1
nombrekeff <manoloedge96@gmail.com>
2020-10-28 10:45:03 +0100

    updated deps, fixed jwt_decode API change

--- projects/caste-client-ng/src/lib/caste-auth.service.spec.ts
+++ projects/caste-client-ng/src/lib/caste-auth.service.spec.ts
@@ -1,11 +1,36 @@
+import { CasteManagementModule } from './caste-management.module';
+import { CasteAuthService } from './caste-auth.service';
 import { TestBed } from '@angular/core/testing';
-import { QbitAuthService } from 'dist/qbit-auth/public-api';
+import { CASTE_AUTH_CONFIG } from '../public-api';
 
-describe('QbitAuthService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+const token =
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2MDM4Nzc4OTcsImV4cCI6MTYwMzk2NDI5Nywicm9sZXMiOlsiUk9MRV9BRE1JTiIsIlJPTEVfU1VQRVJfQURNSU4iLCJST0xFX1VTRVIiXSwidXNlcm5hbWUiOiJtYW5vbG9fc2EyIiwiaXAiOiIxNzYuODMuMjAwLjU0IiwiaWQiOiIyNzk3YTAyNy1lMmM3LTExZWEtYjUyMS0wMjQyMGEwMDAzMGIiLCJhcHBsaWNhdGlvbiI6eyJuYW1lIjoiTG9naWNUcmFkZXJzIChTdGFnZSkiLCJyZWFsbSI6ImRlZmF1bHQifSwicGVybWlzc2lvbnMiOlt7ImdyYW50cyI6WyJBQ0NPVU5UX1dPUktFUiIsIkFDQ09VTlRfTUFOQUdFUiIsIkFDQ09VTlRfVFJBREVSIl0sImFjY291bnQiOnsiaWQiOiIyNzk3ZDJmZC1lMmM3LTExZWEtYjUyMS0wMjQyMGEwMDAzMGIiLCJuYW1lIjoiMzYzNiJ9fSx7ImdyYW50cyI6WyJBQ0NPVU5UX1dPUktFUiIsIkFDQ09VTlRfTUFOQUdFUiJdLCJhY2NvdW50Ijp7ImlkIjoiMWJjY2E1M2UtZTJjOC0xMWVhLWI1MjEtMDI0MjBhMDAwMzBiIiwibmFtZSI6IjMxMzUyIn19XX0.caDFXE2l0_-tSEOUdm8t328mCeSgz11xMM0r-ys8HOj6tnkIFg0GFObC34jpx62Kzq8muim8Re-u_VJ8RffZ7N7yNK9xnb4lO5witKEvgsxdjakNfQjdzp2cPBgXW8dnbrwXDxSPwCV66MfHhyKUn3FMPsIganXJg_x1tD_PqlktqYGeaHyNjZOo0VSg3A8UIEUUY112JHVFxpLDED8t9fjehXTLm4Etjn3mQKPs_SDo1Ebchv31nky-iLdUqOtG4lUcKSkKq7Zp4_5cVrvzDlfzgQ5vLvFmuI6t2hEfQEGupAsK9lioqoZ92xiTGtpUsdvBu6iLX3SYcCs5fpzZywv4bpEC3ZiroFgEL4OuxeEg140U56p5H-hfV5V3Vovr4ZYUeo4waZBlH4f5PsKtahnWFvd08ITa6K5RO9sLfCEYaoYzgUF0gOrsUaKV-TuXRn-mvzj3e8xU1jvv-uc0cSCdfPTorLwyGWviJiAb5iNJpFkHEccXz868-IF2AsjAoT3RugPX8rDbtaDvK-SpDGh8rO60Nuoh3XmTNo1a1oOfSv3x3r7LOBNpXcTJZQM8VAxFiHlyhodbw0Vh_3qXr_kRKmlAKJuiFR0mEvIKmavyr6-YtpuzCRK2l__XysUGpMHV12Y2C-MiPGbjhrvkzDvIr6ua7FHJ9vRq8m-lsMI';
+
+fdescribe('CasteAuthService', () => {
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [CasteManagementModule],
+      providers: [
+        {
+          provide: CASTE_AUTH_CONFIG,
+          useValue: {
+            realm: 'default',
+            url: 'url',
+            baseHeaders: {
+              accept: 'application/ld+json',
+            },
+          },
+        },
+      ],
+    }),
+  );
 
   it('should be created', () => {
-    const service: QbitAuthService = TestBed.get(QbitAuthService);
+    const service: CasteAuthService = TestBed.get(CasteAuthService);
     expect(service).toBeTruthy();
+  });
+
+  it('decodeToken', () => {
+    expect(() => CasteAuthService.decodeToken(token)).not.toThrow();
   });
 });
--- projects/caste-client-ng/src/lib/jwt/jwt-decode.ts
+++ projects/caste-client-ng/src/lib/jwt/jwt-decode.ts
@@ -1,15 +1,11 @@
-import * as jwt_decode_ from 'jwt-decode';
+import jwt_decode from 'jwt-decode';
 import { map } from 'rxjs/internal/operators/map';
 import { LoginResponseRaw, LoginResponse } from '../interfaces/login_response.interface';
 
-// tslint:disable-next-line: variable-name
-const jwt_decode = jwt_decode_;
 
 export function decodeJwt(token: string): LoginResponse {
   const decoded = jwt_decode(token);
   return { ...decoded, token };
 }
 
-export const mapJwtTokenAndAttach = map((data: LoginResponseRaw) =>
-  decodeJwt(data.token)
-);
+export const mapJwtTokenAndAttach = map((data: LoginResponseRaw) => decodeJwt(data.token));
