4daf43d6857a68b814ab1af0d1f87b6452c8315b
nombrekeff <manoloedge96@gmail.com>
2021-02-24 16:39:51 +0100

    added snackbar service

--- projects/qbit-kit-ng/src/public-api.ts
+++ projects/qbit-kit-ng/src/public-api.ts
@@ -7,6 +7,9 @@ export * from './lib/components/superadmin/selectors/admin-account-selector/admi
 export * from './lib/components/superadmin/selectors/current-account-selector/current-account-selector.module';
 export * from './lib/components/superadmin/selectors/permissions-selector/permissions-selector.module';
 
-// Services
+// Qbit components
+export * from './lib/components/qbit/navigation/breadcrumbs/breadcrumbs-selector.module';
 
+// Services
 export * from './lib/services/events.service';
+export * from './lib/services/breadcrumbs.service';
