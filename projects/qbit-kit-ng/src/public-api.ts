48ed507d8c697209e8dde0725e4d4e285246bca3
nombrekeff <manoloedge96@gmail.com>
2021-03-23 12:46:20 +0100

    fix: add missing exports

--- projects/qbit-kit-ng/src/public-api.ts
+++ projects/qbit-kit-ng/src/public-api.ts
@@ -10,6 +10,7 @@ export * from './lib/components/superadmin/selectors/permissions-selector/permis
 // Qbit components
 export * from './lib/components/qbit/navigation/sidemenu/sidemenu.component';
 export * from './lib/components/qbit/navigation/breadcrumbs/breadcrumbs.component';
+export * from './lib/components/qbit/components/table-header/table-header.component';
 
 // Sa components
 export * from './lib/components/superadmin/selectors/account-selector/account-selector.component';
@@ -23,9 +24,12 @@ export * from './lib/services/events.service';
 export * from './lib/services/breadcrumbs.service';
 export * from './lib/services/snackbar.service';
 export * from './lib/services/sidemenu.service';
+
 export * from './lib/snackbar.module';
 export * from './lib/sidemenu.module';
 export * from './lib/breadcrumbs.module';
+export * from './lib/table-header.module';
+
 export * from './lib/sa-common.module';
 export * from './lib/sa-components.module';
 
